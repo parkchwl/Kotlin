8da82fad10e5ad0a598927ae2fa5cdc0231e808b
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-11-06 14:36:04 +0900

    Kotlin
    코틀린으로 백준 알고리즘 [입출력]

---
+++ src/main/kotlin/Operation.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)){
+    val A = nextInt()
+    val B = nextInt()
+    println(A+B)
+    println(A-B)
+    println(A*B)
+    println(A/B)
+    println(A%B)
+
+}

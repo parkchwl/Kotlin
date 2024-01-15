e083706f531f5cd8da875cb1e1d228e107cc2596
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-01-15 14:16:07 +0900

    코틀린 알고리즘 구현
    정렬 단계 - 나이순 정렬

---
+++ src/main/kotlin/Sorting10.kt
@@ -0,0 +1,19 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    val N = scanner.nextInt()
+    val arr = Array(N) { Array(2) { "" } }
+
+    for (i in 0 until N) {
+        arr[i][0] = scanner.next()
+        arr[i][1] = scanner.next() 
+    }
+
+    arr.sortWith(compareBy { it[0].toInt() })
+
+    for (i in 0 until N) {
+        println("${arr[i][0]} ${arr[i][1]}")
+    }
+}

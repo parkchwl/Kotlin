84e5ccceb95e7c081bc5ae130974c12ccab4f5ce
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-04-10 23:50:30 +0900

    알고리즘 스터디 과제 21
    백준 2609 - 최대공약수와 최소공배수

---
+++ src/main/kotlin/AlgorithmStudy21.kt
@@ -0,0 +1,22 @@
+import java.util.*
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val st = StringTokenizer(br.readLine())
+    val N = st.nextToken().toLong()
+    val M = st.nextToken().toLong()
+
+    val gcd = GCD(N, M)
+    val lcm = LCM(N, M, gcd)
+    println(gcd)
+    println(lcm)
+}
+private fun GCD(a: Long, b: Long): Long {
+    if (a % b == 0L) return b
+    return GCD(b, a % b)
+}
+private fun LCM(N: Long, M: Long, gcd: Long): Long {
+    return (N * M) / gcd
+}

9ec8a113a1dfea3e5e4c1c58fa6aeec8a13bd6d8
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-08-02 23:51:24 +0900

    코틀린 알고리즘 구현
    주유소

---
+++ src/main/kotlin/AlgorithmStudy57.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt() - 1
+    val distance = StringTokenizer(readLine()).run { LongArray(n) { nextToken().toLong() } }
+    val price = StringTokenizer(readLine()).run { LongArray(n) { nextToken().toLong() } }
+    var minPrice = price[0]
+    var total = 0L
+    for (i in 0 until n) {
+        minPrice = minOf(minPrice, price[i])
+        total += minPrice * distance[i]
+    }
+    println(total)
+}

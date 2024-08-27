a8ddd23255155a50e0b34088f433e0e55bd2e575
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-08-27 23:57:17 +0900

    코틀린 알고리즘 구현
    25192

---
+++ src/main/kotlin/AlgorithmStudy63.kt
@@ -0,0 +1,15 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    var users = HashSet<String>()
+    var count = 0
+    repeat(n) {
+        readLine().also {
+            if (it == "ENTER") {
+                users = HashSet()
+                return@repeat
+            }
+            if (users.add(it)) count++
+        }
+    }
+    println(count)
+}
